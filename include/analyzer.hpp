2e50e6a7e064f9021c89b1267b63e37b305bfc87
Luke Nguyen <lukenguyen@ucsb.edu>
2025-04-09 16:46:44 -0700

    Define the main class interface

--- include/analyzer.hpp
+++ include/analyzer.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
 
+class Analyzer 
+{
+public: 
+  explicit Analyzer(const std:: string& sourceFile);
+  void run();
+
+private:
+  std::string sourceFile;
+
+  void analyzeTranslation();
+  void checkUnusedVariables();
+  void checkStyleIssues();
+};
